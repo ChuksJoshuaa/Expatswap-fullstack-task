af701640a8464ff720cb4d834c46f4702d506d7c
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2024-03-19 05:07:24 +0100

    update user password validation

--- server/models/User.js
+++ server/models/User.js
@@ -29,16 +29,36 @@ const userSchema = new Mongoose.Schema({
     unique: true,
   },
   password: {
-    type: String,
-    required: [true, "Please provide password"],
-    minLength: 8,
-    validate: {
-      validator: function (v) {
-        return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(v);
+  type: String,
+  required: [true, "Please provide a password"],
+  minLength: [8, "Password must be at least 8 characters long"],
+  validate: [
+    {
+      validator: function(v) {
+        return /[a-z]/.test(v);
       },
-      message: (props) => `${props.value} is not a valid password!`,
+      message: "Password must contain at least one lowercase letter"
     },
-  },
+    {
+      validator: function(v) {
+        return /[A-Z]/.test(v);
+      },
+      message: "Password must contain at least one uppercase letter"
+    },
+    {
+      validator: function(v) {
+        return /\d/.test(v);
+      },
+      message: "Password must contain at least one digit"
+    },
+    {
+      validator: function(v) {
+        return /^(?=.*[a-zA-Z]).{8,}$/.test(v); // This is somewhat redundant due to minLength and checks above but kept for consistency
+      },
+      message: "Password must be at least 8 characters long and include letters"
+    }
+  ]
+},
   dateOfBirth: {
     type: Date,
     required: [true, "Please provide date of birth"],
